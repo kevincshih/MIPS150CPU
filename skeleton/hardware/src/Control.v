55353f2517be7d706d27f40343f5dfdca9a19906
cs150-cw <kevincshih@gmail.com>
2012-10-09 15:31:40 -0700

    Added ALU

--- skeleton/hardware/src/Control.v
+++ skeleton/hardware/src/Control.v
@@ -1,8 +1,29 @@
 module Control(
   input   Clock,
-  input   Reset,);
+  input   Reset,
+  input [5:0] op, funct,
+  input [4:0] branch,
+  output RegWrite, RegDst, AluSrc, Branch, MemWrite, MemtoReg,
+  output [3:0] ALUOp
+);
+`include "Opcode.vh"
+`include "ALUop.vh"
+
   //--|Parameters|--------------------------------------------------------------
 
   //--|Solution|----------------------------------------------------------------
+
+  ALUdec DUT(.funct(funct),
+              .opcode(op),
+              .ALUop(ALUOp));
+
+  always @ (*) begin
+	RegWrite = (op == `RTYPE)||(op == `lw);
+	RegDst = (op == `RTYPE);
+	AluSrc = (op == `lw) || (op == `sw);
+	Branch = (op == `beq);
+	MemWrite = (op == `sw);
+	MemtoReg = 1'b0;
+  end
 
 endmodule
