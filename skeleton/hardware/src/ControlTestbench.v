1b37a0fc021de70bbf1217c403dc5ab6036916b7
Kevin Shih <kevincshih@gmail.com>
2012-10-14 19:26:43 -0700

    added forwarding test

--- skeleton/hardware/src/ControlTestbench.v
+++ skeleton/hardware/src/ControlTestbench.v
@@ -1,6 +1,8 @@
 // If #1 is in the initial block of your testbench, time advances by
 // 1ns rather than 1ps
     `timescale 1ns / 1ps
+    `include "Opcode.vh"
+    `include "ALUop.vh"
 
     module ControlTestBench();
 
@@ -19,11 +21,12 @@ reg[31:0] Address;
 reg[31:0] OldAddress;
 reg branch;
 
-wire RegWrite, RegDst;
-wire[1:0] PCsel;
+wire RegWrite;
+wire[1:0] RegDst, PCsel;
 wire[1:0] AluSelA, AluSelB;
 wire[3:0] ALUop, ByteSel;
-wire WEIM, WEDM, REUART, WEUART, UARTsel, RDsel;
+wire[1:0] UARTsel, RDsel;
+wire WEIM, WEDM, REUART, WEUART;
 
 Control DUT(.Address(Address),
     .OldAddress(OldAddress),
@@ -43,14 +46,96 @@ Control DUT(.Address(Address),
     .UARTsel(UARTsel),
     .RDsel(RDsel));
 
+reg[31:0] REFout;
+reg[31:0] DUTout;
+//reg out1;
+//reg [1:0] out2,out3;
+//reg [1:0] out4, out5;
+//reg [3:0] out6, out7;
+//reg [1:0] out8, out9;
+//reg out10, out11, out12, out13;
+
+task checkOutput;
+input[3:0]test;
+case(test)
+    1: DUTout = RegWrite;
+    2: DUTout = RegDst;
+    3: DUTout = PCsel;
+    4: DUTout = AluSelA;
+    5: DUTout = AluSelB;
+    6: DUTout = ALUop;
+    7: DUTout = ByteSel;
+    8: DUTout = WEIM;
+    9: DUTout = WEDM;
+    10: DUTout = REUART;
+    11: DUTout = WEUART;
+    12: DUTout = UARTsel;
+    13: DUTout = RDsel;
+    default: DUTout = REFout;
+endcase
+if (REFout !== DUTout) begin
+    $display("FAIL: Incorrect result for RegWrite new: %h, old: %h:", Address, OldAddress);
+    $display("\t DUTout: 0x%h, REFout: 0x%h", RegWrite, REFout);
+    $finish();
+end
+else begin
+    $display("PASS: new: %h, old: %h", Address, OldAddress);
+end
+endtask
+
+reg[4:0] zero = 5'b00000;
+reg[4:0] s0 = 5'b10000;
+reg[4:0] s1 = 5'b10001;
+reg[4:0] s2 = 5'b10010;
+reg[4:0] s3 = 5'b10011;
+reg[31:0] nop = 32'b0;
   // Testing logic:
 initial begin
+    Address = {`RTYPE,s0,s0,s0,zero,`ADD};
+    OldAddress = nop;
+    
+    REFout = 1;
     #1;
-
-
-
+    checkOutput(1);
+    #1;
+    
+    REFout = 1;
+    #1;
+    checkOutput(2);
+    #1;
+    
+    REFout = 1;
+    #1;
+    checkOutput(3);
+    #1;
+    
+    REFout = 1;
+    #1;
+    checkOutput(4);
+    #1;
+    
+    REFout = 1;
+    #1;
+    checkOutput(5);
+    #1;
+    
+    Address = {`RTYPE,s0,s0,s0,zero,`ADD};
+    OldAddress = {`RTYPE,s0,s0,s0,zero,`ADD};
+    
+    REFout = 2;
+    #1;
+    checkOutput(4);
+    #1;
+    
+    REFout = 2;
+    #1;
+    checkOutput(5);
+    #1;
+    
     $display("All tests passed!");
     $finish();
+
+
 
 end
 endmodule
