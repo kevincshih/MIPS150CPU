eb1502a0cdc51a43aa39e6d7c5e6e42a15bb912d
Kevin Shih <kevincshih@gmail.com>
2013-01-04 15:01:55 -0800

    final

--- skeleton/hardware/sim/build/work/@line@engine@testbench/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity LineEngineTestbench is
-    generic(
-        HalfCycle       : integer := 5
-    );
-    attribute mti_svvh_generic_type : integer;
-    attribute mti_svvh_generic_type of HalfCycle : constant is 1;
-end LineEngineTestbench;
